4812beef06667c310af064e1146aafd8bbb8724d
gitbreeder <hayun4362@gmail.com>
2025-07-09 08:42:34 +0900

    feat : cafe CRUD

--- src/main/java/com/oopsw/seongsubeancafebackend/service/CafeService.java
+++ src/main/java/com/oopsw/seongsubeancafebackend/service/CafeService.java
@@ -3,9 +3,6 @@ package com.oopsw.seongsubeancafebackend.service;
 import com.oopsw.seongsubeancafebackend.jpa.CafeEntity;
 import com.oopsw.seongsubeancafebackend.vo.CafeRequest;
 import com.oopsw.seongsubeancafebackend.vo.CafeResponse;
-import jakarta.transaction.Transactional;
-import java.util.List;
-import java.util.Map;
 
 public interface CafeService {
 
@@ -15,14 +12,10 @@ public interface CafeService {
   // 2. 카페 단건 조회
   CafeResponse getCafeById(Long cafeId);
 
-
-//  // 11. 카페 수정
-//  CafeResponse updateCafe(Long cafeId, CafeRequest request);
-
-
+  // 3. 카페수정
   CafeResponse updateCafe(Long cafeId, CafeEntity requestEntity);
 
-  // 12. 카페 삭제
+  // 4. 카페 삭제
   void deleteCafe(Long cafeId);
 
 }
